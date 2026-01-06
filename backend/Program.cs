6292b8f31636c12de4b26142b3dd9e7bd68e9338
jj <qjjdlmedel@tip.edu.ph>
2026-01-06 23:46:24 +0800

    feat(backend): infrastructure setup - GlobalExceptionHandler, CORS, and DB Auto-Creation

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,9 +6,21 @@ DotNetEnv.Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddExceptionHandler<TaskManager.Infrastructure.GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -20,9 +32,19 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.EnsureCreated();
+    }
 }
 
+app.UseExceptionHandler();
+
 app.UseHttpsRedirection();
+app.UseCors();
+
 app.MapControllers();
 
 app.Run();
